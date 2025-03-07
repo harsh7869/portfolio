efdf10be3f6c5fbf8bf83665cef86380f2d4c7f3
Harsh Shukla <harshshukla210447@acropolis.in>
2025-03-07 17:05:34 +0530

    add swing when clicked

--- swingMotion.js
+++ swingMotion.js
@@ -4,9 +4,11 @@ document.addEventListener("DOMContentLoaded", function () {
     let bulb = document.querySelector(".mode_light_dark");
     let angle = 0;
     let direction = 1;
-    let interval;
+    // let interval;
+    let interval = null;
+    let isSwinging = false;
     
-    // Set pivot point at the top center
+    // Set  point at the top center
     bulb.style.transformOrigin = "top center";
 
     function swingBulb() {
@@ -27,12 +29,31 @@ document.addEventListener("DOMContentLoaded", function () {
     setTimeout(() => {
 
         clearInterval(interval);  // Stops the swing loop
-
-
+        interval = null;
         bulb.style.transition = "transform 0.5s ease-out"; // Smoothly reset
 
         bulb.style.transform = "rotate(0deg)";  // Set back to normal
     }, 3000);
 
+
+// swing when click happens
+    bulb.addEventListener("click", function () {
+
+        if (isSwinging) return; // Prevent spam clicks
+
+        isSwinging = true; // Mark swinging as active
+        interval = setInterval(swingBulb, 50);
+        
+
+        setTimeout(() => {
+            clearInterval(interval);  // Stops the swing loop
+            interval = null;
+            bulb.style.transition = "transform 0.5s ease-out"; // Smoothly reset
+            
+            bulb.style.transform = "rotate(0deg)";  // Set back to normal
+            isSwinging = false;
+        }, 2000);
+    
+    });
     
 });
