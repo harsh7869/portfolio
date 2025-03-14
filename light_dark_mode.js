4e9fdcdac9807b30f72ac545b5508444b1f986da
Harsh Shukla <harshshukla210447@acropolis.in>
2025-03-14 13:37:48 +0530

    colors for diffrent sections of dark mode added

--- light_dark_mode.js
+++ light_dark_mode.js
@@ -11,3 +11,21 @@ let isFirstVisible = true;
             }
             isFirstVisible = !isFirstVisible;
         }
+
+
+
+        function toggleImage() {
+            document.body.classList.toggle("dark-mode");
+        
+            let onLight = document.getElementById("onlight");
+            let offLight = document.getElementById("offlight");
+        
+            if (document.body.classList.contains("dark-mode")) {
+                onLight.classList.add("hidden");
+                offLight.classList.remove("hidden");
+            } else {
+                onLight.classList.remove("hidden");
+                offLight.classList.add("hidden");
+            }
+        }
+        
